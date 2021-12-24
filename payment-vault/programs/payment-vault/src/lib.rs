b0ce9d01e6b29028b924d16b4d51b43e825411a4
Zano <segfaultdoctor@protonmail.com>
2021-12-24 18:40:15 -0500

    hardcode to 8 mev_payment accounts

--- payment-vault/programs/payment-vault/src/lib.rs
+++ payment-vault/programs/payment-vault/src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(in_band_lifetimes)]
+
 use anchor_lang::prelude::*;
 use std::mem::size_of;
 
@@ -9,10 +11,9 @@ const CONFIG_ACCOUNT_SEED: &'static [u8] = b"CONFIG_ACCOUNT";
 pub mod payment_vault {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, _config_account_bump: u8) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>, _args: InitArgs) -> ProgramResult {
         let cfg = &mut ctx.accounts.config;
         cfg.tip_claimer = ctx.accounts.initial_tip_claimer.key();
-        cfg.admin = ctx.accounts.admin.key();
 
         Ok(())
     }
@@ -22,40 +23,13 @@ pub mod payment_vault {
     /// If this isn't called before the next leader rotation, tips will be transferred
     /// in set_tip_claimer before claimer is updated.
     pub fn claim_tips(ctx: Context<ClaimTips>) -> ProgramResult {
-        let cfg_info = ctx.accounts.config.to_account_info();
-        let tips = Config::calc_tips(cfg_info.lamports())?;
-
-        if tips > 0 {
-            // xfer tips to tip_claimer account
-            **cfg_info.try_borrow_mut_lamports()? -= tips;
-            **ctx.accounts.tip_claimer.try_borrow_mut_lamports()? += tips;
-        }
+        let total_tips = MevPaymentAccount::debit_accounts(ctx.accounts.get_mev_accounts())?;
+        **ctx.accounts.tip_claimer.try_borrow_mut_lamports()? += total_tips;
 
         emit!(TipsClaimed {
             by: ctx.accounts.claimer.key(),
             to: ctx.accounts.tip_claimer.key(),
-            amount: tips,
-        });
-
-        Ok(())
-    }
-
-    #[access_control(check_config_account(&_ctx.accounts.config, _ctx.program_id))]
-    pub fn create_mev_payment_account(
-        _ctx: Context<CreateMEVPaymentAccount>,
-        _mev_payment_bump: u8,
-    ) -> ProgramResult {
-        Ok(())
-    }
-
-    #[access_control(check_config_account(&ctx.accounts.config, ctx.program_id))]
-    pub fn set_admin(ctx: Context<SetAdmin>) -> ProgramResult {
-        let cfg = &mut ctx.accounts.config;
-        cfg.admin = ctx.accounts.new_admin.key();
-
-        emit!(AdminUpdated {
-            new_admin: cfg.admin,
-            old_admin: ctx.accounts.current_admin.key(),
+            amount: total_tips,
         });
 
         Ok(())
@@ -64,21 +38,18 @@ pub mod payment_vault {
     #[access_control(check_config_account(&ctx.accounts.config, ctx.program_id))]
     /// Validator should include this at top of block, at beginning of rotation.
     pub fn set_tip_claimer(ctx: Context<SetTipClaimer>) -> ProgramResult {
-        let cfg_info = ctx.accounts.config.to_account_info();
-        let tips = Config::calc_tips(cfg_info.lamports())?;
+        let total_tips = MevPaymentAccount::debit_accounts(ctx.accounts.get_mev_accounts())?;
 
-        // if Config account has any tips, send to previous tip_claimer account
-        if tips > 0 {
-            msg!("claiming {} lamports to previous validator", tips);
-            // drain account
-            **cfg_info.try_borrow_mut_lamports()? -= tips;
-            // move tips to old account
-            **ctx.accounts.old_tip_claimer.try_borrow_mut_lamports()? += tips;
-
+        if total_tips > 0 {
+            msg!(
+                "transferring {} lamports to previous tip_claimer",
+                total_tips
+            );
+            **ctx.accounts.old_tip_claimer.try_borrow_mut_lamports()? += total_tips;
             emit!(TipsClaimed {
                 by: ctx.accounts.signer.key(),
                 to: ctx.accounts.old_tip_claimer.key(),
-                amount: tips,
+                amount: total_tips,
             });
         }
 
@@ -103,19 +74,104 @@ fn check_config_account(cfg: &Account<Config>, prog_id: &Pubkey) -> ProgramResul
     Ok(())
 }
 
+/// instructions
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Default)]
+pub struct InitArgs {
+    pub mev_seed_1: String,
+    pub mev_seed_2: String,
+    pub mev_seed_3: String,
+    pub mev_seed_4: String,
+    pub mev_seed_5: String,
+    pub mev_seed_6: String,
+    pub mev_seed_7: String,
+    pub mev_seed_8: String,
+    pub mev_bump_1: u8,
+    pub mev_bump_2: u8,
+    pub mev_bump_3: u8,
+    pub mev_bump_4: u8,
+    pub mev_bump_5: u8,
+    pub mev_bump_6: u8,
+    pub mev_bump_7: u8,
+    pub mev_bump_8: u8,
+    pub config_account_bump: u8,
+}
+
 #[derive(Accounts)]
-#[instruction(config_account_bump: u8)]
+#[instruction(args: InitArgs)]
 pub struct Initialize<'info> {
     /// singleton account
     #[account(
         init,
         seeds = [CONFIG_ACCOUNT_SEED],
-        bump = config_account_bump,
+        bump = args.config_account_bump,
         payer = payer,
-        space = Config::LEN,
     )]
     pub config: Account<'info, Config>,
-    pub admin: AccountInfo<'info>,
+    #[account(
+        init,
+        seeds = [args.mev_seed_1.as_bytes()],
+        bump = args.mev_bump_1,
+        payer = payer,
+        space = MevPaymentAccount::LEN,
+    )]
+    pub mev_payment_account_1: Account<'info, MevPaymentAccount>,
+    #[account(
+        init,
+        seeds = [args.mev_seed_2.as_bytes()],
+        bump = args.mev_bump_2,
+        payer = payer,
+        space = MevPaymentAccount::LEN,
+    )]
+    pub mev_payment_account_2: Account<'info, MevPaymentAccount>,
+    #[account(
+        init,
+        seeds = [args.mev_seed_3.as_bytes()],
+        bump = args.mev_bump_3,
+        payer = payer,
+        space = MevPaymentAccount::LEN,
+    )]
+    pub mev_payment_account_3: Account<'info, MevPaymentAccount>,
+    #[account(
+        init,
+        seeds = [args.mev_seed_4.as_bytes()],
+        bump = args.mev_bump_4,
+        payer = payer,
+        space = MevPaymentAccount::LEN,
+    )]
+    pub mev_payment_account_4: Account<'info, MevPaymentAccount>,
+    #[account(
+        init,
+        seeds = [args.mev_seed_5.as_bytes()],
+        bump = args.mev_bump_5,
+        payer = payer,
+        space = MevPaymentAccount::LEN,
+    )]
+    pub mev_payment_account_5: Account<'info, MevPaymentAccount>,
+    #[account(
+        init,
+        seeds = [args.mev_seed_6.as_bytes()],
+        bump = args.mev_bump_6,
+        payer = payer,
+        space = MevPaymentAccount::LEN,
+    )]
+    pub mev_payment_account_6: Account<'info, MevPaymentAccount>,
+    #[account(
+        init,
+        seeds = [args.mev_seed_7.as_bytes()],
+        bump = args.mev_bump_7,
+        payer = payer,
+        space = MevPaymentAccount::LEN,
+    )]
+    pub mev_payment_account_7: Account<'info, MevPaymentAccount>,
+    #[account(
+        init,
+        seeds = [args.mev_seed_8.as_bytes()],
+        bump = args.mev_bump_8,
+        payer = payer,
+        space = MevPaymentAccount::LEN,
+    )]
+    pub mev_payment_account_8: Account<'info, MevPaymentAccount>,
     pub initial_tip_claimer: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
     #[account(mut)]
@@ -125,46 +181,45 @@ pub struct Initialize<'info> {
 #[derive(Accounts)]
 pub struct ClaimTips<'info> {
     #[account(
-        mut,
-        constraint = tip_claimer.key() == config.tip_claimer,
+        constraint = config.tip_claimer == tip_claimer.key(),
     )]
     pub config: Account<'info, Config>,
+    #[account(mut)]
+    pub mev_payment_account_1: Account<'info, MevPaymentAccount>,
+    #[account(mut)]
+    pub mev_payment_account_2: Account<'info, MevPaymentAccount>,
+    #[account(mut)]
+    pub mev_payment_account_3: Account<'info, MevPaymentAccount>,
+    #[account(mut)]
+    pub mev_payment_account_4: Account<'info, MevPaymentAccount>,
+    #[account(mut)]
+    pub mev_payment_account_5: Account<'info, MevPaymentAccount>,
+    #[account(mut)]
+    pub mev_payment_account_6: Account<'info, MevPaymentAccount>,
+    #[account(mut)]
+    pub mev_payment_account_7: Account<'info, MevPaymentAccount>,
+    #[account(mut)]
+    pub mev_payment_account_8: Account<'info, MevPaymentAccount>,
     #[account(mut)]
     pub tip_claimer: AccountInfo<'info>,
     #[account(mut)]
     pub claimer: Signer<'info>,
 }
 
-#[derive(Accounts)]
-#[instruction(mev_payment_bump: u8)]
-pub struct CreateMEVPaymentAccount<'info> {
-    #[account(
-        constraint = config.admin == admin.key(),
-    )]
-    pub config: Account<'info, Config>,
-    #[account(
-        init,
-        seeds = [config.to_account_info().key().as_ref()],
-        bump = mev_payment_bump,
-        payer = admin,
-        space = 8,
-    )]
-    pub mev_payment_account: AccountInfo<'info>,
-    pub system_program: Program<'info, System>,
-    #[account(mut)]
-    pub admin: Signer<'info>,
-}
+impl ClaimTips<'info> {
+    fn get_mev_accounts(&self) -> Vec<AccountInfo<'info>> {
+        let mut accs = Vec::new();
+        accs.push(self.mev_payment_account_1.to_account_info());
+        accs.push(self.mev_payment_account_2.to_account_info());
+        accs.push(self.mev_payment_account_3.to_account_info());
+        accs.push(self.mev_payment_account_4.to_account_info());
+        accs.push(self.mev_payment_account_5.to_account_info());
+        accs.push(self.mev_payment_account_6.to_account_info());
+        accs.push(self.mev_payment_account_7.to_account_info());
+        accs.push(self.mev_payment_account_8.to_account_info());
 
-#[derive(Accounts)]
-pub struct SetAdmin<'info> {
-    #[account(
-        mut,
-        constraint = config.admin == current_admin.key()
-    )]
-    pub config: Account<'info, Config>,
-    pub new_admin: AccountInfo<'info>,
-    #[account(mut)]
-    pub current_admin: Signer<'info>,
+        accs
+    }
 }
 
 #[derive(Accounts)]
@@ -178,26 +233,81 @@ pub struct SetTipClaimer<'info> {
     pub old_tip_claimer: AccountInfo<'info>,
     pub new_tip_claimer: AccountInfo<'info>,
     #[account(mut)]
+    pub mev_payment_account_1: Account<'info, MevPaymentAccount>,
+    #[account(mut)]
+    pub mev_payment_account_2: Account<'info, MevPaymentAccount>,
+    #[account(mut)]
+    pub mev_payment_account_3: Account<'info, MevPaymentAccount>,
+    #[account(mut)]
+    pub mev_payment_account_4: Account<'info, MevPaymentAccount>,
+    #[account(mut)]
+    pub mev_payment_account_5: Account<'info, MevPaymentAccount>,
+    #[account(mut)]
+    pub mev_payment_account_6: Account<'info, MevPaymentAccount>,
+    #[account(mut)]
+    pub mev_payment_account_7: Account<'info, MevPaymentAccount>,
+    #[account(mut)]
+    pub mev_payment_account_8: Account<'info, MevPaymentAccount>,
+    #[account(mut)]
     pub signer: Signer<'info>,
 }
+
+impl SetTipClaimer<'info> {
+    fn get_mev_accounts(&self) -> Vec<AccountInfo<'info>> {
+        let mut accs = Vec::new();
+        accs.push(self.mev_payment_account_1.to_account_info());
+        accs.push(self.mev_payment_account_2.to_account_info());
+        accs.push(self.mev_payment_account_3.to_account_info());
+        accs.push(self.mev_payment_account_4.to_account_info());
+        accs.push(self.mev_payment_account_5.to_account_info());
+        accs.push(self.mev_payment_account_6.to_account_info());
+        accs.push(self.mev_payment_account_7.to_account_info());
+        accs.push(self.mev_payment_account_8.to_account_info());
+
+        accs
+    }
+}
+
+/// accounts
 
 /// Stores program config metadata.
 #[account]
 #[derive(Default)]
 pub struct Config {
-    /// This account has the authority to create and delete a mev_payment_account.
-    admin: Pubkey,
-    /// The account registered by the leader every rotation.
+    /// The account claiming tips from the mev_payment accounts.
     tip_claimer: Pubkey,
 }
 
-impl Config {
+/// Account that searchers will need to tip for their bundles to be accepted.
+/// There will be 8 accounts of this type initialized in order to parallelize bundles.
+#[account]
+#[derive(Default)]
+pub struct MevPaymentAccount {}
+
+impl MevPaymentAccount {
     // add one byte for header
     pub const LEN: usize = 8 + size_of::<Self>();
 
+    fn debit_accounts(accs: Vec<AccountInfo>) -> Result<u64> {
+        let mut total_tips = 0;
+        for acc in accs {
+            total_tips += Self::debit(acc)?;
+        }
+
+        Ok(total_tips)
+    }
+
+    fn debit(acc: AccountInfo) -> Result<u64> {
+        let tips = Self::calc_tips(acc.lamports())?;
+        if tips > 0 {
+            **acc.try_borrow_mut_lamports()? -= tips;
+        }
+        Ok(tips)
+    }
+
     fn calc_tips(total_balance: u64) -> Result<u64> {
         let rent = Rent::get()?;
-        let min_rent = rent.minimum_balance(Config::LEN);
+        let min_rent = rent.minimum_balance(Self::LEN);
         Ok(total_balance - min_rent)
     }
 }
@@ -209,12 +319,6 @@ pub enum ErrorCode {
 }
 
 /// events
-
-#[event]
-pub struct AdminUpdated {
-    new_admin: Pubkey,
-    old_admin: Pubkey,
-}
 
 #[event]
 pub struct TipsClaimed {
--- payment-vault/tests/payment-vault.js
+++ payment-vault/tests/payment-vault.js
@@ -3,9 +3,19 @@ const assert = require( 'assert' )
 const { SystemProgram, Transaction } = anchor.web3
 
 const CONFIG_ACCOUNT_LEN = 8 + 32
+const MEV_PAYMENT_ACCOUNT_LEN = 8
+
+const mevSeed1 = 'MEV_ACCOUNT_1'
+const mevSeed2 = 'MEV_ACCOUNT_2'
+const mevSeed3 = 'MEV_ACCOUNT_3'
+const mevSeed4 = 'MEV_ACCOUNT_4'
+const mevSeed5 = 'MEV_ACCOUNT_5'
+const mevSeed6 = 'MEV_ACCOUNT_6'
+const mevSeed7 = 'MEV_ACCOUNT_7'
+const mevSeed8 = 'MEV_ACCOUNT_8'
 
 describe( 'tests payment_vault', () => {
-    const tipConfigAccount = async ( tipAmount ) => {
+    const sendTip = async ( accountToTip, tipAmount ) => {
         const searcherKP = anchor.web3.Keypair.generate()
         const airDrop = tipAmount * 2
         await provider.connection.confirmTransaction(
@@ -18,7 +28,7 @@ describe( 'tests payment_vault', () => {
         tipTx.add(
             SystemProgram.transfer({
                 fromPubkey: searcherKP.publicKey,
-                toPubkey: configAccount,
+                toPubkey: accountToTip,
                 lamports: tipAmount,
             })
         )
@@ -36,7 +46,15 @@ describe( 'tests payment_vault', () => {
     const paymentVaultProg = anchor.workspace.PaymentVault
     const initializerKeys = anchor.web3.Keypair.generate()
     const globalKeyPairStore = {}
-    let configAccount, configAccountBump
+    let configAccount, configAccountBump,
+        mevPaymentAccount1, mevBump1,
+        mevPaymentAccount2, mevBump2,
+        mevPaymentAccount3, mevBump3,
+        mevPaymentAccount4, mevBump4,
+        mevPaymentAccount5, mevBump5,
+        mevPaymentAccount6, mevBump6,
+        mevPaymentAccount7, mevBump7,
+        mevPaymentAccount8, mevBump8
     before( async () => {
         const [_configAccount, _configAccountBump] = await anchor.web3.PublicKey.findProgramAddress(
             [Buffer.from( 'CONFIG_ACCOUNT', 'utf8' )],
@@ -44,20 +62,55 @@ describe( 'tests payment_vault', () => {
         )
         configAccount = _configAccount
         configAccountBump = _configAccountBump
-        console.log('lets crete')
-        const tx = new Transaction()
-        tx.add(
-            SystemProgram.createAccountWithSeed({
-                fromPubkey: searcherKP.publicKey,
-                toPubkey: configAccount,
-                lamports: tipAmount,
-            })
+        const [_mevPaymentAccount1, _mevBump1] = await anchor.web3.PublicKey.findProgramAddress(
+            [Buffer.from( mevSeed1, 'utf8' )],
+            paymentVaultProg.programId,
         )
-        await anchor.web3.sendAndConfirmTransaction(
-            paymentVaultProg.provider.connection,
-            tipTx,
-            [ searcherKP ],
+        mevPaymentAccount1 = _mevPaymentAccount1
+        mevBump1 = _mevBump1
+        const [_mevPaymentAccount2, _mevBump2] = await anchor.web3.PublicKey.findProgramAddress(
+            [Buffer.from( mevSeed2, 'utf8' )],
+            paymentVaultProg.programId,
         )
+        mevPaymentAccount2 = _mevPaymentAccount2
+        mevBump2 = _mevBump2
+        const [_mevPaymentAccount3, _mevBump3] = await anchor.web3.PublicKey.findProgramAddress(
+            [Buffer.from( mevSeed3, 'utf8' )],
+            paymentVaultProg.programId,
+        )
+        mevPaymentAccount3 = _mevPaymentAccount3
+        mevBump3 = _mevBump3
+        const [_mevPaymentAccount4, _mevBump4] = await anchor.web3.PublicKey.findProgramAddress(
+            [Buffer.from( mevSeed4, 'utf8' )],
+            paymentVaultProg.programId,
+        )
+        mevPaymentAccount4 = _mevPaymentAccount4
+        mevBump4 = _mevBump4
+        const [_mevPaymentAccount5, _mevBump5] = await anchor.web3.PublicKey.findProgramAddress(
+            [Buffer.from( mevSeed5, 'utf8' )],
+            paymentVaultProg.programId,
+        )
+        mevPaymentAccount5 = _mevPaymentAccount5
+        mevBump5 = _mevBump5
+        const [_mevPaymentAccount6, _mevBump6] = await anchor.web3.PublicKey.findProgramAddress(
+            [Buffer.from( mevSeed6, 'utf8' )],
+            paymentVaultProg.programId,
+        )
+        mevPaymentAccount6 = _mevPaymentAccount6
+        mevBump6 = _mevBump6
+        const [_mevPaymentAccount7, _mevBump7] = await anchor.web3.PublicKey.findProgramAddress(
+            [Buffer.from( mevSeed7, 'utf8' )],
+            paymentVaultProg.programId,
+        )
+        mevPaymentAccount7 = _mevPaymentAccount7
+        mevBump7 = _mevBump7
+        const [_mevPaymentAccount8, _mevBump8] = await anchor.web3.PublicKey.findProgramAddress(
+            [Buffer.from( mevSeed8, 'utf8' )],
+            paymentVaultProg.programId,
+        )
+        mevPaymentAccount8 = _mevPaymentAccount8
+        mevBump8 = _mevBump8
+
         await provider.connection.confirmTransaction(
             await provider.connection.requestAirdrop(
                 initializerKeys.publicKey, 100000000000000
@@ -65,15 +118,56 @@ describe( 'tests payment_vault', () => {
             'confirmed',
         )
     })
+
+    // utility function asserting all expected rent exempt accounts are indeed exempt
+    const assertRentExemptAccounts = async () => {
+        let minRentExempt = await paymentVaultProg.provider.connection.getMinimumBalanceForRentExemption( CONFIG_ACCOUNT_LEN )
+        let accInfo = await paymentVaultProg.provider.connection.getAccountInfo( configAccount )
+        assert.equal( accInfo.lamports, minRentExempt )
+
+        minRentExempt = await paymentVaultProg.provider.connection.getMinimumBalanceForRentExemption( MEV_PAYMENT_ACCOUNT_LEN )
+        accInfo = await paymentVaultProg.provider.connection.getAccountInfo( mevPaymentAccount1 )
+        assert.equal( accInfo.lamports, minRentExempt )
+
+        minRentExempt = await paymentVaultProg.provider.connection.getMinimumBalanceForRentExemption( MEV_PAYMENT_ACCOUNT_LEN )
+        accInfo = await paymentVaultProg.provider.connection.getAccountInfo( mevPaymentAccount2 )
+        assert.equal( accInfo.lamports, minRentExempt )
+    }
     it( '#initialize happy path', async () => {
         await paymentVaultProg.rpc.initialize(
-            configAccountBump,
+            {
+                configAccountBump,
+                mevBump1,
+                mevBump2,
+                mevBump3,
+                mevBump4,
+                mevBump5,
+                mevBump6,
+                mevBump7,
+                mevBump8,
+                mevSeed1,
+                mevSeed2,
+                mevSeed3,
+                mevSeed4,
+                mevSeed5,
+                mevSeed6,
+                mevSeed7,
+                mevSeed8,
+            },
             {
               accounts: {
-                config: configAccount,
-                initialTipClaimer: initializerKeys.publicKey,
-                payer: initializerKeys.publicKey,
-                systemProgram: SystemProgram.programId,
+                  config: configAccount,
+                  initialTipClaimer: initializerKeys.publicKey,
+                  payer: initializerKeys.publicKey,
+                  systemProgram: SystemProgram.programId,
+                  mevPaymentAccount1,
+                  mevPaymentAccount2,
+                  mevPaymentAccount3,
+                  mevPaymentAccount4,
+                  mevPaymentAccount5,
+                  mevPaymentAccount6,
+                  mevPaymentAccount7,
+                  mevPaymentAccount8,
               },
               signers: [initializerKeys],
             },
@@ -81,25 +175,31 @@ describe( 'tests payment_vault', () => {
         const configState = await paymentVaultProg.account.config.fetch( configAccount )
         assert.equal( configState.tipClaimer.toString(), initializerKeys.publicKey.toString() )
     })
-    it( '#change_tip_claimer with no funds succeeds', async () => {
+    it( '#set_tip_claimer with 0 total tips succeeds', async () => {
         let configState = await paymentVaultProg.account.config.fetch( configAccount )
         const oldTipClaimer = configState.tipClaimer
         const newTipClaimer = anchor.web3.Keypair.generate()
         globalKeyPairStore[ newTipClaimer.publicKey ] = newTipClaimer
-        await paymentVaultProg.rpc.changeTipClaimer(
+        await paymentVaultProg.rpc.setTipClaimer(
             {
               accounts: {
-                oldTipClaimer,
-                newTipClaimer: newTipClaimer.publicKey,
-                config: configAccount,
-                signer: initializerKeys.publicKey,
+                  oldTipClaimer,
+                  newTipClaimer: newTipClaimer.publicKey,
+                  config: configAccount,
+                  signer: initializerKeys.publicKey,
+                  mevPaymentAccount1,
+                  mevPaymentAccount2,
+                  mevPaymentAccount3,
+                  mevPaymentAccount4,
+                  mevPaymentAccount5,
+                  mevPaymentAccount6,
+                  mevPaymentAccount7,
+                  mevPaymentAccount8,
               },
               signers: [initializerKeys],
             },
         )
-        const minRentExempt = await paymentVaultProg.provider.connection.getMinimumBalanceForRentExemption( CONFIG_ACCOUNT_LEN )
-        const configInfo = await paymentVaultProg.provider.connection.getAccountInfo( configAccount )
-        assert.equal( configInfo.lamports, minRentExempt )
+        await assertRentExemptAccounts()
         configState = await paymentVaultProg.account.config.fetch( configAccount )
         assert.equal( configState.tipClaimer.toString(), newTipClaimer.publicKey.toString())
     })
@@ -112,6 +212,14 @@ describe( 'tests payment_vault', () => {
                       claimer: initializerKeys.publicKey,
                       config: configAccount,
                       tipClaimer: wrongTipClaimer,
+                      mevPaymentAccount1,
+                      mevPaymentAccount2,
+                      mevPaymentAccount3,
+                      mevPaymentAccount4,
+                      mevPaymentAccount5,
+                      mevPaymentAccount6,
+                      mevPaymentAccount7,
+                      mevPaymentAccount8,
                   },
                   signers: [initializerKeys],
               },
@@ -124,50 +232,71 @@ describe( 'tests payment_vault', () => {
     it( '#claim_tips moves funds to correct account', async () => {
         const claimer = anchor.web3.Keypair.generate()
         const tipAmount = 1000000
-        await tipConfigAccount( tipAmount )
+        await sendTip( mevPaymentAccount1, tipAmount )
+        await sendTip( mevPaymentAccount2, tipAmount )
+        const totalTip = tipAmount * 2
+
         let configState = await paymentVaultProg.account.config.fetch( configAccount )
         const tipClaimer = configState.tipClaimer
+        const tipClaimerLamportsBefore =
+            (( await paymentVaultProg.provider.connection.getAccountInfo( tipClaimer )) || { lamports: 0 }).lamports
         await paymentVaultProg.rpc.claimTips(
             {
                 accounts: {
                     tipClaimer,
                     claimer: claimer.publicKey,
                     config: configAccount,
+                    mevPaymentAccount1,
+                    mevPaymentAccount2,
+                    mevPaymentAccount3,
+                    mevPaymentAccount4,
+                    mevPaymentAccount5,
+                    mevPaymentAccount6,
+                    mevPaymentAccount7,
+                    mevPaymentAccount8,
                 },
               signers: [ claimer ],
             },
         )
-        const minRentExempt = await paymentVaultProg.provider.connection.getMinimumBalanceForRentExemption( CONFIG_ACCOUNT_LEN )
-        configInfo = await paymentVaultProg.provider.connection.getAccountInfo( configAccount )
-        // check that account is still rent exempt
-        assert.equal( configInfo.lamports, minRentExempt )
-        const tipClaimerLamports = ( await paymentVaultProg.provider.connection.getAccountInfo( tipClaimer )).lamports
-        assert.equal( tipClaimerLamports, tipAmount )
+        await assertRentExemptAccounts()
+        const tipClaimerLamportsAfter =
+            ( await paymentVaultProg.provider.connection.getAccountInfo( tipClaimer )).lamports
+        assert.equal( tipClaimerLamportsAfter - tipClaimerLamportsBefore, totalTip )
     })
-    it( '#change_tip_claimer transfers funds to previous tip_claimer', async () => {
-        const tipAmount = 100000
-        await tipConfigAccount( tipAmount )
+    it( '#set_tip_claimer transfers funds to previous tip_claimer', async () => {
+        const tipAmount = 10000000
+        await sendTip( mevPaymentAccount1, tipAmount )
+        await sendTip( mevPaymentAccount2, tipAmount )
+        const totalTip = tipAmount * 2
+
         let configState = await paymentVaultProg.account.config.fetch( configAccount )
         const oldTipClaimer = configState.tipClaimer
-        const oldTipClaimerBalance = ( await paymentVaultProg.provider.connection.getAccountInfo( oldTipClaimer )).lamports
+        const oldTipClaimerBalanceBefore =
+            ( await paymentVaultProg.provider.connection.getAccountInfo( oldTipClaimer )).lamports
         const newTipClaimer = anchor.web3.Keypair.generate()
         const newLeader = anchor.web3.Keypair.generate()
-        await paymentVaultProg.rpc.changeTipClaimer(
+        await paymentVaultProg.rpc.setTipClaimer(
             {
                 accounts: {
                     oldTipClaimer,
                     newTipClaimer: newTipClaimer.publicKey,
                     config: configAccount,
                     signer: newLeader.publicKey,
+                    mevPaymentAccount1,
+                    mevPaymentAccount2,
+                    mevPaymentAccount3,
+                    mevPaymentAccount4,
+                    mevPaymentAccount5,
+                    mevPaymentAccount6,
+                    mevPaymentAccount7,
+                    mevPaymentAccount8,
                 },
                 signers: [newLeader],
             },
         )
-        const minRentExempt = await paymentVaultProg.provider.connection.getMinimumBalanceForRentExemption( CONFIG_ACCOUNT_LEN )
-        configInfo = await paymentVaultProg.provider.connection.getAccountInfo( configAccount )
-        // check that account is still rent exempt
-        assert.equal( configInfo.lamports, minRentExempt )
-        const updatedOldTipClaimerBalance = ( await paymentVaultProg.provider.connection.getAccountInfo( oldTipClaimer )).lamports
-        assert.equal( updatedOldTipClaimerBalance, tipAmount + oldTipClaimerBalance )
+        await assertRentExemptAccounts()
+        const oldTipClaimerBalanceAfter =
+            ( await paymentVaultProg.provider.connection.getAccountInfo( oldTipClaimer )).lamports
+        assert.equal( oldTipClaimerBalanceAfter, totalTip + oldTipClaimerBalanceBefore )
     })
 })
