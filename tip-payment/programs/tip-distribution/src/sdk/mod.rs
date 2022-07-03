18e66009c3e908ae1613e110387c61faa163cae9
Zanyar Sherwani <seg@Zanyars-MacBook-Pro.local>
2022-07-03 15:34:09 -0400

    add config account derivation fn helper

--- tip-payment/programs/tip-distribution/src/sdk/mod.rs
+++ tip-payment/programs/tip-distribution/src/sdk/mod.rs
@@ -2,7 +2,7 @@ pub mod instruction;
 
 use anchor_lang::{prelude::Pubkey, solana_program::clock::Epoch};
 
-use crate::TipDistributionAccount;
+use crate::{Config, TipDistributionAccount};
 
 pub fn derive_tip_distribution_account_address(
     tip_distribution_program_id: &Pubkey,
@@ -17,4 +17,8 @@ pub fn derive_tip_distribution_account_address(
         ],
         tip_distribution_program_id,
     )
+}
+
+pub fn derive_config_account_address(tip_distribution_program_id: &Pubkey) -> (Pubkey, u8) {
+    Pubkey::find_program_address(&[Config::SEED], tip_distribution_program_id)
 }
