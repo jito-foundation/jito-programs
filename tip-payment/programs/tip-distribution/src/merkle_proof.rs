0bd16e9bfd181bf3776e92cf38965de262dcc453
0xspl.iff <bill@jito.wtf>
2022-07-26 02:57:31 -0700

    modified verify

--- tip-payment/programs/tip-distribution/src/merkle_proof.rs
+++ tip-payment/programs/tip-distribution/src/merkle_proof.rs
@@ -1,5 +1,7 @@
 //! copy-pasta from [here](https://github.com/saber-hq/merkle-distributor/blob/ac937d1901033ecb7fa3b0db22f7b39569c8e052/programs/merkle-distributor/src/merkle_proof.rs)
 
+use anchor_lang::solana_program;
+
 /// This function deals with verification of Merkle trees (hash trees).
 /// Direct port of https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0/contracts/cryptography/MerkleProof.sol
 /// Returns true if a `leaf` can be proved to be a part of a Merkle tree
@@ -17,6 +19,24 @@ pub fn verify(proof: Vec<[u8; 32]>, root: [u8; 32], leaf: [u8; 32]) -> bool {
             // Hash(current element of the proof + current computed hash)
             computed_hash =
                 anchor_lang::solana_program::keccak::hashv(&[&proof_element, &computed_hash]).0;
+        }
+    }
+    // Check if the computed hash (root) is equal to the provided root
+    computed_hash == root
+}
+
+// the construction library uses [1] to represent intermediate nodes
+pub fn verify_fixed(proof: Vec<[u8; 32]>, root: [u8; 32], leaf: [u8; 32]) -> bool {
+    let mut computed_hash = leaf;
+    for proof_element in proof.into_iter() {
+        if computed_hash <= proof_element {
+            // Hash(current computed hash + current element of the proof)
+            computed_hash =
+                solana_program::hash::hashv(&[&[1u8], &computed_hash, &proof_element]).to_bytes();
+        } else {
+            // Hash(current element of the proof + current computed hash)
+            computed_hash =
+                solana_program::hash::hashv(&[&[1u8], &proof_element, &computed_hash]).to_bytes();
         }
     }
     // Check if the computed hash (root) is equal to the provided root
