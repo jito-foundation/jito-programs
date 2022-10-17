81fe1abecf2e8f90b015696a228133d9a2cc3d46
buffalu <85544055+buffalu@users.noreply.github.com>
2022-10-17 15:55:31 -0500

    Improve simplicity + reliability of autosnapshot (#48)
    Remove autoclaim. Everything runs in the same process bc was running into race conditions before where autoclaim + autosnapshot run at same time causing issues.
    Remove claim-mev. We discussed it previously about putting claim-mev into jito-programs or jito-solana, but having the json structs copied-pasted between repos annoying and error-prone. See Separate MEV tip related workflow jito-solana#161 for updated scripts

--- tip-payment/scripts/autosnapshot.sh
+++ tip-payment/scripts/autosnapshot.sh
@@ -1,24 +1,19 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # This script creates a snapshot for the last confirmed slot
 # in the previous epoch if one doesn't already exist.
+# After creating a snapshot, it creates a snapshot of metadata
+# and merkle roots before uploading them on-chain
+# NOTE: this file depends on binaries being built in jito-solana
 
-set -e
-
-DIR="$(cd "$(dirname "$0")" && pwd)"
-source ./${DIR}/utils.sh
+# error out, unset variables are errors, and echo commands
+set -eux
 
 RPC_URL=$1
-HOST_NAME=$2
 
 # make sure all env vars are set for this script
-check_env() {
+check_env_vars_set() {
   if [ -z "$RPC_URL" ]; then
-    echo "Must pass RPC URL as first arg"
-    exit 1
-  fi
-
-  if [ -z "$HOST_NAME" ]; then
-    echo "Must pass host name as second arg"
+    echo "RPC_URL must be set"
     exit 1
   fi
 
@@ -36,62 +31,278 @@ check_env() {
     echo "SOLANA_CLUSTER must be set"
     exit 1
   fi
+
+  if [ -z "$TIP_DISTRIBUTION_PROGRAM_ID" ]; then
+    echo "TIP_DISTRIBUTION_PROGRAM_ID must be set"
+    exit 1
+  fi
+
+  if [ -z "$TIP_PAYMENT_PROGRAM_ID" ]; then
+    echo "TIP_PAYMENT_PROGRAM_ID must be set"
+    exit 1
+  fi
+
+  if [ -z "$KEYPAIR" ]; then
+    echo "KEYPAIR must be set"
+    exit 1
+  fi
+
+  if [ -z "$LEDGER_DIR" ]; then
+    echo "LEDGER_DIR must be set"
+    exit 1
+  fi
 }
 
+# read epoch info from RPC endpoint
+get_epoch_info() {
+  local rpc_url=$1
+
+  local epoch_info
+
+  epoch_info=$(curl "$rpc_url" -X POST -H "Content-Type: application/json" -d '
+      {"jsonrpc":"2.0","id":1, "method":"getEpochInfo"}
+    ')
+  if [ -z "$epoch_info" ]; then
+    echo "ERROR Unable to fetch epoch info."
+    exit 1
+  fi
+  echo "$epoch_info"
+}
+
+# gets the snapshot filename, assuming it's present
+# cant be known ahead of time because the snapshot filename includes a hash
+get_snapshot_filename() {
+  local snapshot_dir=$1
+  local snapshot_slot=$2
+
+  local snapshot_file
+
+  snapshot_file=$(ls "$snapshot_dir" | { grep ".tar.zst" || true; } | { grep "$snapshot_slot" || true; })
+  echo "$snapshot_file"
+}
+
+# creates a snapshot for the given slot and returns the filename
 create_snapshot_for_slot() {
   local snapshot_slot=$1
+  local snapshot_dir=$2
+  local ledger_location=$3
+
+  local snapshot_file
 
   # shellcheck disable=SC2012
-  local snapshot_file=$(ls "$SNAPSHOT_DIR" | { grep ".tar.zst" || true; } | { grep "$snapshot_slot" || true; })
+  RUST_LOG=info solana-ledger-tool -l "$ledger_location" create-snapshot "$snapshot_slot"
+  # ledger-tool by default updates snapshots in the existing ledger directory
+  # and prunes old full/incremental snapshots. copy it out to our snapshot
+  # directory when finished creating.
+  cp "$ledger_location"*"$snapshot_slot"* "$snapshot_dir"
+
+  # snapshot file should exist now, grab the filename here (snapshot-$slot-$hash.tar.zst)
+  snapshot_file=$(get_snapshot_filename "$snapshot_dir" "$snapshot_slot")
+
+  echo "$snapshot_file"
+}
+
+generate_stake_meta() {
+  local slot=$1
+  local snapshot_dir=$2
+  local stake_meta_filename=$3
+  local tip_distribution_program_id=$4
+  local tip_payment_program_id=$5
+
+  rm -rf "$snapshot_dir"/stake-meta.accounts || true
+  rm -rf "$snapshot_dir"/tmp* || true
+  rm -r /tmp/.tmp* || true # calculate hash stuff stored here
+
+  RUST_LOG=info solana-stake-meta-generator \
+    --ledger-path "$snapshot_dir" \
+    --tip-distribution-program-id "$tip_distribution_program_id" \
+    --out-path "$snapshot_dir/$stake_meta_filename" \
+    --snapshot-slot "$slot" \
+    --tip-payment-program-id "$tip_payment_program_id"
+
+  rm -rf "$snapshot_dir"/stake-meta.accounts || true
+  rm -rf "$snapshot_dir"/tmp* || true
+  rm -r /tmp/.tmp* || true # calculate hash stuff stored here
+}
+
+generate_merkle_trees() {
+  local stake_meta_filepath=$1
+  local merkle_tree_filepath=$2
+  RUST_LOG=info solana-merkle-root-generator \
+    --stake-meta-coll-path "$stake_meta_filepath" \
+    --out-path "$merkle_tree_filepath"
+}
+
+claim_tips() {
+  local merkle_trees_path=$1
+  local rpc_url=$2
+  local tip_distribution_program_id=$3
+  local keypair_path=$4
+
+  RUST_LOG=info solana-claim-mev-tips \
+    --merkle-trees-path "$merkle_trees_path" \
+    --rpc-url "$rpc_url" \
+    --tip-distribution-program-id "$tip_distribution_program_id" \
+    --keypair-path "$keypair_path"
+}
+
+get_gcloud_path() {
+  local solana_cluster=$1
+  local epoch=$2
+  local file_name=$3
+
+  local upload_path
+
+  upload_path="gs://jito-$solana_cluster/$epoch/$(hostname)/$file_name"
+
+  echo "$upload_path"
+}
+
+get_filepath_in_gcloud() {
+  local upload_path=$1
+
+  local file_uploaded
+
+  file_uploaded=$(gcloud storage ls "$upload_path" | { grep "$upload_path" || true; })
+
+  echo "$file_uploaded"
+}
+
+upload_file_to_gcloud() {
+  local filepath=$1
+  local gcloud_path=$2
+
+  gcloud storage cp "$filepath" "$gcloud_path"
+}
+
+upload_merkle_roots() {
+  local merkle_root_path=$1
+  local keypair_path=$2
+  local rpc_url=$3
+  local tip_distribution_program_id=$4
+
+  RUST_LOG=info \
+    solana-merkle-root-uploader \
+    --merkle-root-path "$merkle_root_path" \
+    --keypair-path "$keypair_path" \
+    --rpc-url "$rpc_url" \
+    --tip-distribution-program-id "$tip_distribution_program_id"
+}
+
+main() {
+  local epoch_info
+  local previous_epoch_final_slot
+  local snapshot_file
+  local snapshot_gcloud_path
+  local maybe_stake_meta
+  local stake_meta_gcloud_path
+  local stake_meta_filename
+  local merkle_tree_filename
+  local merkle_tree_filepath
+  local maybe_merkle_tree
+
+  check_env_vars_set
+
+  # make sure snapshot location exists and has a genesis file in it
+  mkdir -p "$SNAPSHOT_DIR"
+  cp "$LEDGER_DIR"/genesis.bin "$SNAPSHOT_DIR"
+
+  # ---------------------------------------------------------------------------
+  # Read epoch info off RPC and calculate previous epoch + previous epoch's last slot
+  # ---------------------------------------------------------------------------
+
+  epoch_info=$(get_epoch_info "$RPC_URL")
+  current_epoch=$(echo "$epoch_info" | jq .result.epoch)
+  last_epoch=$((current_epoch - 1))
+  current_absolute_slot=$(echo "$epoch_info" | jq .result.absoluteSlot)
+  current_slot_index=$(echo "$epoch_info" | jq .result.slotIndex)
+  epoch_start_slot=$((current_absolute_slot - current_slot_index))
+  previous_epoch_final_slot="$((epoch_start_slot - 1))"
+
+  echo "epoch_info: $epoch_info"
+  echo "previous_epoch_final_slot: $previous_epoch_final_slot"
+
+  # ---------------------------------------------------------------------------
+  # Take snapshot and upload to gcloud
+  # ---------------------------------------------------------------------------
+
+  snapshot_file=$(get_snapshot_filename "$SNAPSHOT_DIR" "$previous_epoch_final_slot")
   if [ -z "$snapshot_file" ]; then
-    clean_old_snapshot_files
-    echo "Didn't find snapshot for slot $snapshot_slot, creating..."
-    RUST_LOG=info solana-ledger-tool -l "$LEDGER_LOCATION" create-snapshot "$snapshot_slot"
-    # ledger-tool by default updates snapshots in the existing ledger directory
-    # and prunes old full/incremental snapshots. copy it out to our snapshot
-    # directory when finished creating.
-    cp "$LEDGER_LOCATION"*"$snapshot_slot"* "$SNAPSHOT_DIR"
+    echo "creating snapshot at slot $snapshot_slot"
+
+    # note: make sure these filenames match everywhere else in the file
+    rm "$SNAPSHOT_DIR/snapshot-*.tar.zst" || true
+    rm "$SNAPSHOT_DIR/stake-meta-*.json" || true
+    rm "$SNAPSHOT_DIR/merkle-tree-*.json" || true
+
+    snapshot_file=$(create_snapshot_for_slot "$previous_epoch_final_slot" "$SNAPSHOT_DIR" "$LEDGER_DIR")
   else
-    echo "Found snapshot, nothing to do."
+    echo "snapshot file already exists: $SNAPSHOT_DIR/$snapshot_file"
   fi
 
-  # shellcheck disable=SC2012
-  SNAPSHOT_FILE=$(ls "$SNAPSHOT_DIR" | { grep ".tar.zst" || true; } | { grep "$snapshot_slot" || true; })
-}
-
-upload_snapshot() {
-  local epoch_info=$1
-  local snapshot_file=$2
-
-  local current_epoch=$(echo "$epoch_info" | jq .result.epoch)
-  local last_epoch=$((current_epoch - 1))
-  local upload_path="gs://jito-$SOLANA_CLUSTER/$last_epoch/$HOST_NAME/$snapshot_file"
-  local snapshot_uploaded=$(gcloud storage ls "$upload_path" | { grep "$upload_path" || true; })
-
-  if [ -z "$snapshot_uploaded" ]; then
-    echo "Snapshot not found in gcp bucket, uploading now."
-    echo "snapshot_file: $snapshot_file"
-    echo "upload_path: $upload_path"
-    gcloud storage cp $SNAPSHOT_DIR/"$snapshot_file" "$upload_path"
+  snapshot_gcloud_path=$(get_gcloud_path "$SOLANA_CLUSTER" "$last_epoch" "$snapshot_file")
+  snapshot_in_gcloud=$(get_filepath_in_gcloud "$snapshot_gcloud_path") || true
+  if [ -z "$snapshot_in_gcloud" ]; then
+    echo "uploading $SNAPSHOT_DIR/$snapshot_file to gcloud path $snapshot_gcloud_path"
+    upload_file_to_gcloud "$SNAPSHOT_DIR/$snapshot_file" "$snapshot_gcloud_path"
   else
-    echo "Snapshot already uploaded to gcp."
+    echo "snapshot file already uploaded to gcloud at: $snapshot_in_gcloud"
   fi
-}
 
-clean_old_snapshot_files() {
-  # shellcheck disable=SC2012
-  maybe_snapshot=$(ls "$SNAPSHOT_DIR"snapshot* 2>/dev/null | { grep -E "snapshot" || true; })
-  if [ -z "$maybe_snapshot" ]; then
-    echo "No snapshots to clean up."
+  # ---------------------------------------------------------------------------
+  # Load in snapshot, produce stake metadata, and upload to gcloud
+  # ---------------------------------------------------------------------------
+
+  stake_meta_filename=stake-meta-"$previous_epoch_final_slot".json
+  stake_meta_filepath="$SNAPSHOT_DIR/$stake_meta_filename"
+  maybe_stake_meta=$(ls "$stake_meta_filepath" 2>/dev/null || true)
+  if [ -z "$maybe_stake_meta" ]; then
+    echo "Running stake-meta-generator for slot $previous_epoch_final_slot"
+    generate_stake_meta "$previous_epoch_final_slot" "$SNAPSHOT_DIR" "$stake_meta_filename" "$TIP_DISTRIBUTION_PROGRAM_ID" "$TIP_PAYMENT_PROGRAM_ID"
   else
-    rm "$maybe_snapshot"
+    echo "stake-meta already exists: $stake_meta_filepath"
   fi
+
+  stake_meta_gcloud_path=$(get_gcloud_path "$SOLANA_CLUSTER" "$last_epoch" "$stake_meta_filename")
+  stake_meta_in_gcloud=$(get_filepath_in_gcloud "$stake_meta_gcloud_path") || true
+  if [ -z "$stake_meta_in_gcloud" ]; then
+    echo "uploading $stake_meta_filepath to gcloud path $stake_meta_gcloud_path"
+    upload_file_to_gcloud "$stake_meta_filepath" "$stake_meta_gcloud_path"
+  else
+    echo "stake meta already uploaded to gcloud at: $stake_meta_in_gcloud"
+  fi
+
+  # ---------------------------------------------------------------------------
+  # Produce merkle tree, upload to gcloud, and upload merkle roots on-chain for
+  # the provided keypairs
+  # ---------------------------------------------------------------------------
+
+  merkle_tree_filename=merkle-tree-"$previous_epoch_final_slot".json
+  merkle_tree_filepath="$SNAPSHOT_DIR/$merkle_tree_filename"
+  maybe_merkle_tree=$(ls "$merkle_tree_filepath" 2>/dev/null || true)
+  if [ -z "$maybe_merkle_tree" ]; then
+    echo "Running merkle-root-generator for slot $previous_epoch_final_slot"
+    generate_merkle_trees "$stake_meta_filepath" "$merkle_tree_filepath"
+  else
+    echo "stake-meta already exists at: $merkle_tree_filepath"
+  fi
+
+  merkle_tree_gcloud_path=$(get_gcloud_path "$SOLANA_CLUSTER" "$last_epoch" "$merkle_tree_filename")
+  merkle_tree_in_gcloud=$(get_filepath_in_gcloud "$merkle_tree_gcloud_path") || true
+  if [ -z "$merkle_tree_in_gcloud" ]; then
+    echo "uploading $merkle_tree_filepath to gcloud path $merkle_tree_gcloud_path"
+    upload_file_to_gcloud "$merkle_tree_filepath" "$merkle_tree_gcloud_path"
+  else
+    echo "merkle tree already uploaded to gcloud at: $merkle_tree_gcloud_path"
+  fi
+
+  upload_merkle_roots "$merkle_tree_filepath" "$KEYPAIR" "$RPC_URL" "$TIP_DISTRIBUTION_PROGRAM_ID"
+
+  # ---------------------------------------------------------------------------
+  # Claim MEV tips
+  # ---------------------------------------------------------------------------
+
+  claim_tips "$merkle_tree_filepath" "$RPC_URL" "$TIP_DISTRIBUTION_PROGRAM_ID" "$KEYPAIR"
 }
 
-check_env
-
-fetch_epoch_info "$RPC_URL"
-calculate_epoch_end_slot "$EPOCH_INFO"
-echo "last confirmed slot in previous epoch: $EPOCH_FINAL_SLOT"
-create_snapshot_for_slot "$EPOCH_FINAL_SLOT"
-upload_snapshot "$EPOCH_INFO" "$SNAPSHOT_FILE"
+main
