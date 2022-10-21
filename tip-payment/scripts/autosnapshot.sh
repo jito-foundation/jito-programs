a4b3e73f0246c480dc8922cb1f790c29f1ef37eb
Lucas B <lucas@jito.wtf>
2022-10-20 19:36:22 -0500

    wip

--- tip-payment/scripts/autosnapshot.sh
+++ tip-payment/scripts/autosnapshot.sh
@@ -102,11 +102,6 @@ create_snapshot_for_slot() {
       exit $exit_status
   fi
 
-  # ledger-tool by default updates snapshots in the existing ledger directory
-  # and prunes old full/incremental snapshots. copy it out to our snapshot
-  # directory when finished creating.
-  cp "$ledger_location/snapshot-$snapshot_slot"* "$snapshot_dir"
-
   # snapshot file should exist now, grab the filename here (snapshot-$slot-$hash.tar.zst)
   snapshot_file=$(get_snapshot_filename "$snapshot_dir" "$snapshot_slot")
 
