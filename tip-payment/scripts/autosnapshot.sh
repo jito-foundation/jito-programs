af901f3a3094090ba24012d49aaf2821e776e03a
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2023-02-21 06:59:29 -0800

    fix params

--- tip-payment/scripts/autosnapshot.sh
+++ tip-payment/scripts/autosnapshot.sh
@@ -19,7 +19,7 @@ post_slack_message() {
 }
 
 main() {
-  if ! "$DIR"/autosnapshot_inner.sh; then
+  if ! "$DIR"/autosnapshot_inner.sh "$@"; then
     NUM_LOG_LINES=10
     LOG_SNIPPET=$(journalctl -u autosnapshot --pager-end --lines $NUM_LOG_LINES)
     post_slack_message "$SLACK_APP_TOKEN" "$SLACK_CHANNEL" "autosnapshot failed. journalctl log snippet:
@@ -28,4 +28,4 @@ main() {
   fi
 }
 
-main
+main "$@"
