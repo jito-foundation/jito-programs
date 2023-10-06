4c630b0af7acaa3c631addf479fa3042b9d5c362
Lucas B <lucas@jito.wtf>
2023-10-05 19:29:46 -0500

    Adjust clippy for jito-solana building

--- mev-programs/programs/vote-state/src/lib.rs
+++ mev-programs/programs/vote-state/src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(unused_doc_comments)]
 #![allow(dead_code)]
 //! This code was mostly copy-pasta'd from [here](https://github.com/solana-labs/solana/blob/df128573127c324cb5b53634a7e2d77427c6f2d8/programs/vote/src/vote_state/mod.rs#L1).
 //! In all current releases [VoteState] is defined in the `solana-vote-program` crate which is not compatible
