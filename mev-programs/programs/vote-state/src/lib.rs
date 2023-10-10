b913019c55f42420cb98f244d226da2f02106e4d
buffalu <85544055+buffalu@users.noreply.github.com>
2023-10-09 19:39:08 -0500

    Add new vote account for timely vote credits (#101)
    Add new vote account for TVC.
    Updating solana validator versions for CI.
    Update dependencies for solana-* in Cargo.toml
    Comment out solana-verify for now as it's not working with v1.16

--- mev-programs/programs/vote-state/src/lib.rs
+++ mev-programs/programs/vote-state/src/lib.rs
@@ -22,7 +22,7 @@ pub struct Lockout {
     pub confirmation_count: u32,
 }
 
-#[derive(Default, Deserialize)]
+#[derive(Clone, Default, Deserialize)]
 struct AuthorizedVoters {
     authorized_voters: BTreeMap<Epoch, Pubkey>,
 }
@@ -54,6 +54,7 @@ pub struct BlockTimestamp {
 #[derive(Deserialize)]
 pub enum VoteStateVersions {
     V0_23_5(Box<VoteState0_23_5>),
+    V1_14_11(Box<VoteState1_14_11>),
     Current(Box<VoteState>),
 }
 
@@ -67,38 +68,51 @@ impl VoteStateVersions {
                 Box::new(VoteState {
                     node_pubkey: state.node_pubkey,
 
-                    /// the signer for withdrawals
                     authorized_withdrawer: state.authorized_withdrawer,
 
-                    /// percentage (0-100) that represents what part of a rewards
-                    ///  payout should be given to this VoteAccount
                     commission: state.commission,
 
-                    votes: state.votes,
+                    votes: Self::landed_votes_from_lockouts(state.votes),
 
                     root_slot: state.root_slot,
-                    /// the signer for vote transactions
+
                     authorized_voters,
-                    /// history of prior authorized voters and the epochs for which
-                    /// they were set, the bottom end of the range is inclusive,
-                    /// the top of the range is exclusive
+
                     prior_voters: CircBuf::default(),
 
-                    /// history of how many credits earned by the end of each epoch
-                    ///  each tuple is (Epoch, credits, prev_credits)
-                    epoch_credits: state.epoch_credits,
+                    epoch_credits: state.epoch_credits.clone(),
 
-                    /// most recent timestamp submitted with a vote
-                    last_timestamp: state.last_timestamp,
+                    last_timestamp: state.last_timestamp.clone(),
                 })
             }
+            VoteStateVersions::V1_14_11(state) => Box::new(VoteState {
+                node_pubkey: state.node_pubkey,
+                authorized_withdrawer: state.authorized_withdrawer,
+                commission: state.commission,
+
+                votes: Self::landed_votes_from_lockouts(state.votes),
+
+                root_slot: state.root_slot,
+
+                authorized_voters: state.authorized_voters.clone(),
+
+                prior_voters: state.prior_voters,
+
+                epoch_credits: state.epoch_credits,
+
+                last_timestamp: state.last_timestamp,
+            }),
             VoteStateVersions::Current(state) => state,
         }
+    }
+
+    fn landed_votes_from_lockouts(lockouts: VecDeque<Lockout>) -> VecDeque<LandedVote> {
+        lockouts.into_iter().map(|lockout| lockout.into()).collect()
     }
 }
 
 #[derive(Deserialize)]
-pub struct VoteState {
+pub struct VoteState1_14_11 {
     /// the node that votes in this account
     pub node_pubkey: Pubkey,
 
@@ -175,6 +189,81 @@ pub struct VoteState0_23_5 {
     /// most recent timestamp submitted with a vote
     #[serde(skip_deserializing)]
     pub last_timestamp: BlockTimestamp,
+}
+
+#[derive(Deserialize)]
+pub struct VoteState {
+    /// the node that votes in this account
+    pub node_pubkey: Pubkey,
+
+    /// the signer for withdrawals
+    #[serde(skip_deserializing)]
+    pub authorized_withdrawer: Pubkey,
+    /// percentage (0-100) that represents what part of a rewards
+    ///  payout should be given to this VoteAccount
+    #[serde(skip_deserializing)]
+    pub commission: u8,
+
+    #[serde(skip_deserializing)]
+    pub votes: VecDeque<LandedVote>,
+
+    // This usually the last Lockout which was popped from self.votes.
+    // However, it can be arbitrary slot, when being used inside Tower
+    #[serde(skip_deserializing)]
+    pub root_slot: Option<Slot>,
+
+    /// the signer for vote transactions
+    #[serde(skip_deserializing)]
+    authorized_voters: AuthorizedVoters,
+
+    /// history of prior authorized voters and the epochs for which
+    /// they were set, the bottom end of the range is inclusive,
+    /// the top of the range is exclusive
+    #[serde(skip_deserializing)]
+    prior_voters: CircBuf<(Pubkey, Epoch, Epoch)>,
+
+    /// history of how many credits earned by the end of each epoch
+    ///  each tuple is (Epoch, credits, prev_credits)
+    #[serde(skip_deserializing)]
+    pub epoch_credits: Vec<(Epoch, u64, u64)>,
+
+    /// most recent timestamp submitted with a vote
+    #[serde(skip_deserializing)]
+    pub last_timestamp: BlockTimestamp,
+}
+
+#[derive(Deserialize)]
+pub struct LandedVote {
+    // Latency is the difference in slot number between the slot that was voted on (lockout.slot) and the slot in
+    // which the vote that added this Lockout landed.  For votes which were cast before versions of the validator
+    // software which recorded vote latencies, latency is recorded as 0.
+    pub latency: u8,
+    pub lockout: Lockout,
+}
+
+impl LandedVote {
+    pub fn slot(&self) -> Slot {
+        self.lockout.slot
+    }
+
+    pub fn confirmation_count(&self) -> u32 {
+        self.lockout.confirmation_count
+    }
+}
+
+impl From<LandedVote> for Lockout {
+    fn from(landed_vote: LandedVote) -> Self {
+        landed_vote.lockout
+    }
+}
+
+impl From<Lockout> for LandedVote {
+    fn from(lockout: Lockout) -> Self {
+        Self {
+            latency: 0,
+            lockout,
+        }
+    }
 }
 
 impl VoteState {
