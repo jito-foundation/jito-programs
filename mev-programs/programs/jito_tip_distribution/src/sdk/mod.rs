7780b24680f712507d4dcefadabba9060e8d90f1
Evan Batsell <ebatsell@gmail.com>
2025-03-05 10:10:50 -0500

    New dir name

---
+++ mev-programs/programs/jito_tip_distribution/src/sdk/mod.rs
@@ -0,0 +1,24 @@
+pub mod instructions;
+
+use anchor_lang::{prelude::Pubkey, solana_program::clock::Epoch};
+
+use crate::{Config, TipDistributionAccount};
+
+pub fn derive_tip_distribution_account_address(
+    tip_distribution_program_id: &Pubkey,
+    vote_pubkey: &Pubkey,
+    epoch: Epoch,
+) -> (Pubkey, u8) {
+    Pubkey::find_program_address(
+        &[
+            TipDistributionAccount::SEED,
+            vote_pubkey.to_bytes().as_ref(),
+            epoch.to_le_bytes().as_ref(),
+        ],
+        tip_distribution_program_id,
+    )
+}
+
+pub fn derive_config_account_address(tip_distribution_program_id: &Pubkey) -> (Pubkey, u8) {
+    Pubkey::find_program_address(&[Config::SEED], tip_distribution_program_id)
+}
